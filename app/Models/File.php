8b80fda476a34dcc218baef2ede0591799e521cd
Belal Salah <belal.salah259@gmail.com>
2026-01-26 21:08:37 +0200

    adding file.php logic

--- app/Models/File.php
+++ app/Models/File.php
@@ -5,8 +5,10 @@ namespace App\Models;
 use App\Traits\HasCreatorAndUpdator;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Kalnoy\Nestedset\NodeTrait;
+use Illuminate\Support\Str;
 
 class File extends Model
 {
@@ -16,17 +18,26 @@ class File extends Model
     {
         return $this->created_by === $userId;
     }
+    public function user():BelongsTo{
+        return $this->belongsTo(User::class,'created_by');
+    }
+    public function parent():BelongsTo{
+        return $this->belongsTo(File::class,'parent_id');
+    }
 
-    // protected $fillable = [
-    //     'name',
-    //     'path',
-    //     'is_folder',
-    //     'mime',
-    //     'size',
-    //     'created_by',
-    //     'updated_by',
-    //     'parent_id',
-    //     '_lft',
-    //     '_rgt',
-    // ];
+
+
+    public function isRoot():bool{
+        return $this->parent_id === null;
+    }
+    protected static function boot(){
+        parent::boot();
+        static::creating(function ($model){
+           if(!$model->parent){
+            return;
+           }
+            $model->path = (!$model->parent->isRoot()?$model->parent->path.'/':'').Str::slug($model->name);
+        });
+    }
+
 }
