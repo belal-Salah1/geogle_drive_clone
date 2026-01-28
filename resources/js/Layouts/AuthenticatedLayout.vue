488111b56ce47db2ca67d2e1afa150f547a61cc3
Belal Salah <belal.salah259@gmail.com>
2026-01-28 17:35:05 +0200

    edit on vue.json to make it generate default comments when creating any script tag

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -1,9 +1,22 @@
+
 <script setup>
 import Navigation from '@/Components/app/Navigation.vue';
 import SearchForm from '@/Components/app/SearchForm.vue';
 import UserSettingDropDown from '@/Components/app/UserSettingsDropDown.vue';
 import { emitter, FILE_UPLOAD_STARTED } from '@/event-bus';
-import { onMounted } from 'vue';
+import { onMounted, ref } from 'vue';
+
+const dragOver = ref(false);
+
+function handleDrop(event) {
+  const files = event.dataTransfer.files;
+  uploadFiles(files);
+}
+function onDragover(event) {
+}
+function onDragleave(event) {
+
+}
 
 function uploadFiles(files) {
   console.log('file upload started listener in layout', files);
@@ -17,7 +30,12 @@ onMounted(() => {
 <template>
   <div class="h-screen bg-gray-50 flex w-full gap-4">
     <Navigation />
-    <main class="flex flex-col flex-1 px-4 overflow-hidden">
+    <main
+      class="flex flex-col flex-1 px-4 overflow-hidden"
+      @drop.prevent="handleDrop"
+      @dragover.prevent="onDragover"
+      @dragleave.prevent="onDragleave"
+    >
       <div class="flex items-center justify-between w-full">
         <SearchForm />
         <UserSettingDropDown />
