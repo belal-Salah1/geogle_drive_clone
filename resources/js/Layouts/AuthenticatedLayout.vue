1c0c216c39c9246b48d68c82a4cd64b6d09452d8
Belal Salah <belal.salah259@gmail.com>
2026-01-28 18:20:49 +0200

    adding drap files functionality in front

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -1,4 +1,3 @@
-
 <script setup>
 import Navigation from '@/Components/app/Navigation.vue';
 import SearchForm from '@/Components/app/SearchForm.vue';
@@ -8,14 +7,17 @@ import { onMounted, ref } from 'vue';
 
 const dragOver = ref(false);
 
+function onDragover() {
+  dragOver.value = true;
+}
+function onDragleave() {
+  dragOver.value = false;
+}
 function handleDrop(event) {
+  dragOver.value = false;
   const files = event.dataTransfer.files;
+  if (files.length === 0) return;
   uploadFiles(files);
-}
-function onDragover(event) {
-}
-function onDragleave(event) {
-
 }
 
 function uploadFiles(files) {
@@ -32,18 +34,37 @@ onMounted(() => {
     <Navigation />
     <main
       class="flex flex-col flex-1 px-4 overflow-hidden"
+      :class="dragOver ? 'dropzone' : ''"
       @drop.prevent="handleDrop"
       @dragover.prevent="onDragover"
       @dragleave.prevent="onDragleave"
     >
-      <div class="flex items-center justify-between w-full">
-        <SearchForm />
-        <UserSettingDropDown />
-      </div>
+      <template v-if="dragOver">
+        <div class="text-gray text-center py-8">Drop files here to upload</div>
+      </template>
+      <template v-else>
+        <div class="flex items-center justify-between w-full">
+          <SearchForm />
+          <UserSettingDropDown />
+        </div>
 
-      <div class="flex-1 flex flex-col overflow-hidden">
-        <slot />
-      </div>
+        <div class="flex-1 flex flex-col overflow-hidden">
+          <slot />
+        </div>
+      </template>
     </main>
   </div>
 </template>
+
+<style scoped>
+.dropzone {
+  width: 100%;
+  height: 100%;
+  color: #8d8d8d;
+  border: 2px dashed #3b82f6;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #e0f2fe;
+}
+</style>
